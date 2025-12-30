f22d7344b1b39ef557258949ac50e18d767e0916
shanliuling <zhangjinlinqq@gmail.com>
2025-12-30 18:17:52 +0800

    feat: 添加初始主页

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,41 +1,7 @@
-// 'use client'
-
-import { useTRPC } from '@/trpc/client'
-import { caller, getQueryClient, trpc } from '@/trpc/server'
-import { dehydrate, HydrationBoundary, useQuery } from '@tanstack/react-query'
-import { Client } from './client'
-import { Suspense } from 'react'
+'use client'
 
 const Page = async () => {
-  // const trpc = useTRPC()
-  // const { data } = useQuery(
-  //   trpc.createAI.queryOptions({
-  //     text: 'hellow阿斯顿撒',
-  //   })
-  // )
-  //
-
-  const data = await caller.createAI({
-    text: 'hellow阿斯顿撒',
-  })
-  // 预取
-  const queryClient = getQueryClient()
-  void queryClient.prefetchQuery(
-    trpc.createAI.queryOptions({ text: 'zhangjinlin prefetch' })
-  ) //
-
-  return (
-    <div>
-      {JSON.stringify(data)}
-      {/* {JSON.stringify(data2)} */}
-      {/* HydrationBoundary：  */}
-      <HydrationBoundary state={dehydrate(queryClient)}>
-        <Suspense fallback={<p>loading......</p>}>
-          <Client></Client>
-        </Suspense>
-      </HydrationBoundary>
-    </div>
-  )
+  return <div>hello</div>
 }
 
 export default Page
