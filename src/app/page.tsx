8f4f5f30f1bc755850aa4bd1b3d2a2f226c289bf
shanliuling <1362913430@qq.com>
2026-02-23 22:27:22 +0800

    feat: 实现“创建项目”流程，支持随机项目名生成及成功后的路由跳转

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,38 +4,41 @@ import { Button } from '@/components/ui/button'
 import { Input } from '@/components/ui/input'
 import { useTRPC } from '@/trpc/client'
 import { useMutation, useQuery } from '@tanstack/react-query'
+import { useRouter } from 'next/navigation'
 import { useState } from 'react'
 import { toast } from 'sonner'
 
 const Page = () => {
   const trpc = useTRPC() // 获取TRPC客户端
-  const { data: messages } = useQuery(trpc.messages.getMany.queryOptions())
-  const createMessage = useMutation(
-    trpc.messages.create.mutationOptions({
-      onSuccess: () => {
-        toast.success('消息已经创建')
+
+  const createProject = useMutation(
+    trpc.projects.create.mutationOptions({
+      onSuccess: (data) => {
+        router.push(`/projects/${data.id}`)
+        toast.success('项目已创建 🎉')
+      },
+      onError: (error) => {
+        toast.error(error.message)
       },
     }),
   ) // 使用TRPC的invoke mutation
   const [value, setValue] = useState('')
+  const router = useRouter()
   return (
-    <div className="p-4 max-w-7xl mx-auto">
-      <Input
-        value={value}
-        onChange={(e) => {
-          setValue(e.target.value)
-        }}></Input>
-      <Button
-        disabled={createMessage.isPending}
-        onClick={() => {
-          createMessage.mutate({ value: value }) //触发后台任务事件
-        }}>
-        点击
-      </Button>
-      <div>
-        {messages?.map((message) => (
-          <div key={message.id}>{message.content}</div>
-        ))}
+    <div className="h-screen w-screen flex items-center justify-center">
+      <div className="max-w-7xl mx-auto flex items-center flex-col gap-y-4 justify-center">
+        <Input
+          value={value}
+          onChange={(e) => {
+            setValue(e.target.value)
+          }}></Input>
+        <Button
+          disabled={createProject.isPending}
+          onClick={() => {
+            createProject.mutate({ value: value }) //触发后台任务事件
+          }}>
+          点击
+        </Button>
       </div>
     </div>
   )
--- src/trpc/routers/_app.ts
+++ src/trpc/routers/_app.ts
@@ -1,8 +1,10 @@
+import { projectsRouter } from '@/modules/projects/server/procedures'
 import { createTRPCRouter } from '../init'
 
-import { messagesRouter } from '@/modules/server/procedures'
+import { messagesRouter } from '@/modules/messages/server/procedures'
 export const appRouter = createTRPCRouter({
   messages: messagesRouter,
+  projects: projectsRouter,
 })
 // 导出 API 的类型定义
 export type AppRouter = typeof appRouter
